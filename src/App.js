6bff6df60a6eea6881ea446d91a05f25d2233aad
samuelochaba <109516201+samuelochaba@users.noreply.github.com>
2022-09-04 21:24:14 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import "./App.css";
 import PackagesData from "./pages/PackagesData";
-import Barcode from "./pages/Barcode";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Barcode />} />
-        <Route path="/barcode" element={<Barcode />} />
+        <Route path="/" element={<Home />} />
 
         <Route path="/packages-data" element={<PackagesData />}></Route>
       </Routes>

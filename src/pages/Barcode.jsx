1a810fbea53f5f691b166bdbf870a3194e8d2d10
samuelochaba <109516201+samuelochaba@users.noreply.github.com>
2022-09-04 12:38:22 +0100

    barcode

--- src/pages/Barcode.jsx
+++ src/pages/Barcode.jsx
@@ -1,18 +1,28 @@
 import React from "react";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
+// import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import { BarcodeScanner } from "react-barcode-scanner";
 
 const Barcode = () => {
   const [data, setData] = React.useState("Not Found");
   return (
+    // <>
+    //   <BarcodeScannerComponent
+    //     width={500}
+    //     height={500}
+    //     onUpdate={(err, result) => {
+    //       if (result) setData(result.text);
+    //       else setData("Not Found");
+    //     }}
+    //     facingMode="user"
+    //   />
+    //   <p>{data}</p>
+    // </>
     <>
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
+      <BarcodeScanner
+        onCapture={(data) => {
+          console.log(data);
+          setData(data);
         }}
-        facingMode="user"
       />
       <p>{data}</p>
     </>
